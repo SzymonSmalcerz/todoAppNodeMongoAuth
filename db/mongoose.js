ded256b93a9f477fc08ade17b3f1eb39f09a6dab
Szymon Smalcerz <smalcerzszymonn@gmail.com>
2017-09-27 14:38:35 +0200

    heroku

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGOLAB_URI || "mongodb://localhost:27017/TodoAppVol2",{useMongoClient: true});
+mongoose.connect(process.env.MONGOLAB_URI || "mongodb://localhost:27017/TodoAppVol2",{useMongoClient: true});
 
 module.exports = {mongoose};
